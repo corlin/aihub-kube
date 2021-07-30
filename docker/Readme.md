cc24b34ac765aca4e028f43e495c8f922200e630
corlin <me@corlin.cn>
2021-07-30 15:05:07 +0800

    更新

---
+++ docker/Readme.md
@@ -0,0 +1,35 @@
+https://hub.docker.com/r/d1egoaz/docker-kafka-monitor/
+
+kafka
+wurstmeister/kafka:2.11-2.0.0
+https://www.github.com/wurstmeister/kafka-docker
+library/zookeeper:3.4.13
+https://hub.docker.com/_/zookeeper
+https://github.com/31z4/zookeeper-docker/
+
+image: sheepkiller/kafka-manager@sha256:615f3b99d38aba2d5fdb3fb750a5990ba9260c8fb3fd29c7e776e8c150518b78
+https://www.github.com/sheepkiller/kafka-manager-docker
+image: d1egoaz/docker-kafka-monitor@sha256:307e59f9db70a34ab659a5714010a805440b65d1b948cb79f6564a4567ca0f2c
+
+1.1.x https://github.com/linkedin/kafka-monitor/releases/tag/1.1.0 
+2.0.x https://github.com/linkedin/kafka-monitor/releases/tag/2.0.0 
+2.0.1 https://github.com/linkedin/kafka-monitor/releases/tag/2.0.1
+
+image: martinthiele/kafka-mirrormaker@sha256:f5bf26b8dd4002093a92b957fc41fa381332ab4a0047498e732c51a9f84542f5
+https://github.com/martinthiele/docker-kafka-mirrormaker
+
+corlin/flink:1.13.0-scala_2.11
+
+
+
+
+corlin/kafka-kraft
+
+docker login
+
+docker build -t corlin/flink flink/
+docker build -t corlin/docker docker/
+
+docker build
+
+
---
+++ v02/deploy.sh
@@ -0,0 +1,53 @@
+minikube start --memory=6144 --cpus=4
+kubectl apply -f 00-namespace/
+kubectl apply -f 01-zookeeper/
+kubectl apply -f 02-kafka/
+kubectl apply -f 03-yahoo-kafka-manager/
+kubectl apply -f 04-kafka-monitor/
+
+
+kubectl apply -f 05-kafka-mirrormaker/
+
+
+watch -n 1 kubectl -n aihub1 get deployments
+watch -n 1 kubectl -n aihub1 get statefulsets
+watch -n 1 kubectl -n aihub1 get services
+watch -n 1 kubectl -n aihub1 get pods
+watch -n 1 minikube service list --namespace aihub1
+
+kafka-0                          1/1     Running   0          14m
+kafka-1                          1/1     Running   0          14m
+kafka-2                          1/1     Running   0          14m
+kafka-manager-7b8fb9f6c6-4c7lg   1/1     Running   0          14m
+kafka-monitor-6795444469-cm6sd   1/1     Running   0          14m
+zookeeper-7b69697846-clg8s       1/1     Running   0          14m
+
+
+kubectl -n aihub1 exec kafka-monitor-6795444469-cm6sd  -i -t bash
+
+kubectl port-forward -n aihub1 --address 0.0.0.0 kafka-monitor-6795444469-cm6sd 8000:8000
+
+
+### Monitor end-to-end a single cluster + JMX
+
+Run the following:
+```bash
+KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.rmi.port=9999 -Djava.rmi.server.hostname=127.0.0.1 " \
+  exec ./bin/end-to-end-test.sh --broker-list kafka-0.kafka.aihub1.svc.cluster.local:9092,kafka-1.kafka.aihub1.svc.cluster.local:9092,kafka-2.kafka.aihub1.svc.cluster.local:9092 --zookeeper zookeeper-service.aihub1.svc.cluster.local:2181 \
+  --topic testtopic
+```
+
+### Monitor a single cluster + JMX using more detailed configuration kafka-monitor.properties
+
+```bash
+KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.rmi.port=9999 -Djava.rmi.server.hostname=127.0.0.1 " \
+  exec ./bin/kafka-monitor-start.sh config/kafka-monitor.properties
+```
+
+
+kubectl port-forward -n aihub1 --address 0.0.0.0 kafka-manager-7b8fb9f6c6-4c7lg 9000:9000
+
+
+kubectl -n aihub1 exec kafka-0 -- tail -f /opt/kafka/logs/state-change.log
+kubectl -n aihub1 exec kafka-0 -- tail -f /opt/kafka/logs/server.log
+kubectl -n aihub1 exec kafka-0 -- tail -f /opt/kafka/logs/controller.log
