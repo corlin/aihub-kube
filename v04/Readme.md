1b68a596778444ac122d221f5731f844d1fc19b5
corlin <me@corlin.cn>
2021-08-10 19:04:58 +0800

    pv相关

---
+++ v04/Readme.md
@@ -0,0 +1,26 @@
+## 操作
+
+kubectl describe pv 
+## nfs共享存储
+```shell
+yum install nfs-utils rpcbind -y
+mkdir -p /data/nfs && chmod 755 /data/nfs/
+cat /etc/exports
+/data/nfs  *(rw,sync,no_root_squash)
+systemctl start rpcbind nfs && systemctl enable rpcbind nfs
+
+
+```
+## 参数
+
+◎ ReadWriteOnce（RWO）：读写权限，并且只能被单个Node挂
+载。
+◎ ReadOnlyMany（ROX）：只读权限，允许被多个Node挂载。
+◎ ReadWriteMany（RWX）：读写权限，允许被多个Node挂载。
+
+## 参考
+https://kubernetes.io/docs/concepts/storage/persistent-volumes/
+https://time.geekbang.org/column/article/42698
+https://minikube.sigs.k8s.io/docs/commands/node/
+NFS
+https://blog.51cto.com/u_13777088/2471778
---
+++ v04/deploy.sh
@@ -0,0 +1,50 @@
+kubectl apply -f 00-namespace/
+kubectl apply -f 01-zookeeper/
+kubectl apply -f 02-kafka/
+kubectl apply -f 03-yahoo-kafka-manager/
+kubectl apply -f 04-kafka-monitor/
+kubectl apply -f 05-kafka-mirrormaker/
+kubectl apply -f 06-flink/
+
+kubectl get all  -n aihub
+kubectl get event -n aihub
+
+watch  kubectl get po -n aihub
+
+kubectl port-forward -n aihub --address 0.0.0.0 flink-jobmanager-779d9559c7-tmlvd 8081:8081
+kubectl port-forward -n aihub --address 0.0.0.0 kafka-manager-7b8fb9f6c6-ztz4c 9000:9000
+
+kubectl -n aihub exec kafka-0 -- tail -f /opt/kafka/logs/state-change.log
+kubectl -n aihub exec kafka-0 -- tail -f /opt/kafka/logs/server.log
+kubectl -n aihub exec kafka-0 -- tail -f /opt/kafka/logs/controller.log
+
+
+kubectl exec flink-jobmanager-797c75fbc7-8mxhk -n aihub  -i -t -- bash -il
+
+/home/corlin/flink-1.13.1/bin/flink run -p 1  -m localhost:8081  -c com.alibaba.alink.GBDTExample  /home/corlin/demo/alinktest.jar
+/home/corlin/flink-1.13.1/bin/flink run -p 1  -m localhost:8081  -c cn.creditease.KafkaTestSource  /home/corlin/demo/alinktest.jar
+
+kubectl -n aihub exec kafka-0  -i -t -- bash -il
+ kubectl describe  kafka -n aihub
+
+kubectl port-forward service/kafka --address 0.0.0.0 9092:9092 -n aihub
+
+kubectl exec -it kafka-0 -n aihub -- /bin/sh
+
+kafka-topics.sh --create --topic gbdtout --partitions 3 --replication-factor 3 --bootstrap-server localhost:9092
+kafka-topics.sh --create --topic gbdt --partitions 3 --replication-factor 3 --bootstrap-server localhost:9092
+kafka-topics.sh --describe --topic gbdt --bootstrap-server localhost:9092
+
+
+kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic gbdt
+kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic gbdtout
+
+/home/flink/flink-1.13.1/bin/flink run -p 1  -m localhost:8081  -c cn.creditease.GbdtStreamTest  /home/flink/alinktest.jar
+
+/home/flink/flink-1.13.1/bin/flink run -p 1  -m localhost:8081  -c cn.creditease.KafkaTestSource  /home/corlin/alinktest.jar
+
+{"education":"HS-grad","education_num":9,"occupation":"Adm-clerical","race":"White","sex":"Male","label":"<=50K","hours_per_week":35,"fnlwgt":198613,"capital_loss":0,"native_country":"United-States","marital_status":"Married-civ-spouse","workclass":"Private","relationship":"Husband","age":31,"capital_gain":0}
+{"education":"Masters","education_num":14,"occupation":"Prof-specialty","race":"White","sex":"Female","label":"<=50K","hours_per_week":40,"fnlwgt":104834,"capital_loss":1669,"native_country":"United-States","marital_status":"Never-married","workclass":"Private","relationship":"Not-in-family","age":26,"capital_gain":0}
+
+
+kafka-console-producer.sh --bootstrap-server localhost:9092 --topic gbdt
